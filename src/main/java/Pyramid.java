805b773ebef907fa0cc80550e9082e67faab4040
Dabana Intenque <Dabanaluintenque55@gmail.com>
2024-04-06 18:40:06 +0000

    Coding Challenge Pyramid
    Done!

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,20 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+
+
+        String pyramid = "";
+
+        for (int i = 1; i <= n; i ++){
+
+            for(int j = 0; j < i; j ++){
+
+                pyramid += "*";
+            }
+
+            pyramid += "\n";
+        }
+        
+        return pyramid;
     }
 }
